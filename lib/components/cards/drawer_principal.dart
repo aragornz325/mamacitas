f8bc4a9fb249b0352bad02708b62035c1c481852
chivo <rodrigo.m.quintero@gmail.com>
2024-01-17 19:28:13 -0300

    trabajando en el drawer

--- lib/components/cards/drawer_principal.dart
+++ lib/components/cards/drawer_principal.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 Widget DrawerPrincipal(BuildContext context) {
   return Drawer(
     backgroundColor: Colors.cyan,
-    child: Column(
+    child: ListView(
       children: <Widget>[
         LineasDrawer(
           texto: 'configura mamaCitas',
@@ -13,6 +13,17 @@ Widget DrawerPrincipal(BuildContext context) {
           texto: "tu saldo",
           icono: Icons.monetization_on,
         ),
+        SizedBox(height: 16), // Espacio entre las dos primeras líneas y la última
+        Container(
+          color: Colors.grey,
+          child: const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Align(
+              alignment: Alignment.bottomLeft,
+              child: Text('todos los derechos reservados'),
+            ),
+          ),
+        )
       ],
     ),
   );
